e70c85e69faae73f689695ae36fe198884e2aa9e
Maria Meir <pigyarx@gmail.com>
2023-08-13 20:59:37 -0400

    tooltip social icons

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+
+  const tooltips = document.querySelectorAll(".tooltip");
+tooltips.forEach((tooltip) => {
+  const tooltipText = tooltip.querySelector(".tooltiptext");
+  tooltip.addEventListener("mousemove", (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    tooltipText.style.top = mouseY + "px";
+    tooltipText.style.left = mouseX + "px";
+  });
+  tooltip.addEventListener("mouseenter", () => {
+    tooltipText.style.visibility = "visible";
+    tooltipText.style.opacity = "1";
+  });
+  tooltip.addEventListener("mouseleave", () => {
+    tooltipText.style.visibility = "hidden";
+    tooltipText.style.opacity = "0";
+  });
+});
